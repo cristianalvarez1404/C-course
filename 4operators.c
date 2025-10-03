02ead5b76a79d9d381794c86cbb932d0b7661fda
cristianalvarez1404 <cristianandres1496@gmail.com>
2025-10-03 15:11:52 -0500

    adding operators and formats

---
+++ 4operators.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    //aritmetic operators = + - * / % ++ --
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    //z = x + y;
+    //z = x - y;
+    //z = x * y;
+    //z = x / y;
+    //z = x % y;
+    //x++;
+
+    x = x + 2;
+    x += 2;
+    x -= 2;
+    x *= 2;
+    x /= 2;
+
+
+    printf("%d", x);
+
+    return 0;
+}
+
