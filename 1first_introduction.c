91be605ff3aec3564de34b573297261f9f99f492
cristian <cristianalvarez1496@gmail.com>
2025-10-02 20:50:21 -0500

    programmin in C

---
+++ 1first_introduction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+  // This is my first program! :D
+
+  /*
+    This
+    is
+    also
+    a
+    comment
+  */
+ 
+  printf("I like pizza!\n");
+  printf("It's really good!");
+
+  return 0;
+}
---
+++ 2variables.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+  // variable = A reusable container for a value.
+  //Behaves as if it were the value it contains.
+
+  /*
+    int age = 25;
+    int year = 2025;
+    int quantity = 2;
+
+    printf("You are %d years old\n", age);
+    printf("The year is %d\n", year);
+    printf("You have ordered %d x items ", quantity);
+  */
+
+  /*
+    float gpa = 2.5;
+    float price = 19.99;
+    float temperature = -10.1;
+
+    printf("Your gpa is %.1f\n", gpa);
+    printf("The price is $%.2f\n",price);
+    printf("The temperature is %.1f\n",temperature);
+  */
+
+  /*
+    double pi = 3.14159265358979;
+    double e = 2.7182818284590;
+
+    printf("The value of pi is %.15lf\n",pi);
+    printf("The value of e is %.15lf\n", e);
+  */
+
+  /*
+    char grade = 'F';
+    char symbol = '!';
+    char currency = '$';
+
+    printf("Your grade is %c\n", grade);
+    printf("Your favorite symbol is %c\n",symbol);
+    printf("The currency is %c\n",currency);
+  */
+
+  /*
+    char name[] = "Bro Code";
+    char food[] = "pizza";
+    char email[] = "fake123@gmail.com";
+
+    printf("Hello %s\n", name);
+    printf("Your favorite food is %s\n", food);
+    printf("Your email is %s\n", email);
+  */
+
+  /*
+    bool isOnline = true;
+    bool isStudent = true;
+    bool forSale = false;
+
+    if(isOnline){
+      //printf("%d", isOnline);
+      printf("You are online");
+    }else {
+      printf("You are offline");
+    }
+
+    if(isStudent){
+      printf("You are a student");
+    }else{
+      printf("You are't a student");
+    }
+
+    if(forSale){
+      printf("That item is for sale");
+    }
+    else {
+      printf("That item is not for sale");
+    }
+  */
+
+
+  return 0;
+}
